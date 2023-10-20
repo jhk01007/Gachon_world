a4a8c7234420f98f5dc1a0da1b264a044ed97dd4
Yoon Sang Jo <jsy440720@gmail.com>
2023-10-21 01:50:02 +0900

    Modify UI

--- build/gachon_hero.js
+++ build/gachon_hero.js
@@ -1,47 +1,18 @@
+
 var keyhero = {};
+
+var bar1, bar2, bar3, bar4, bar5;
+var audioName, gameName;
+var combo = 0;
+
 
 keyhero.board = [];
 keyhero.notePos = [-2.85, -1.4, 0, 1.4, 2.85];
 keyhero.keyPressed = [0, 0, 0, 0, 0]; // f g h j k
 keyhero.sounds = ['sound1', 'sound2', 'sound3', 'sound4', 'sound5', 'slide'];
 keyhero.colors = ['#00e500', '#ff0000', '#ffff00', '#0000ff', '#ffa500'];
-keyhero.position = [5, 4, 3, 2, 1, 2, 3, 5, 5, 5, 4, 5, 3, 5, 1, 2,
-     4, 5, 2, 1, 2, 2, 4, 3, 1, 4, 2, 3, 3, 2, 2, 1, 1, 1, 3, 2, 2,
-      3, 4, 4, 2, 4, 2, 3, 2, 5, 4, 2, 1, 2, 4, 3, 4, 2, 2, 1, 4, 5,
-       4, 4, 4, 4, 1, 4, 3, 2, 5, 4, 4, 5, 3, 5, 1, 3, 2, 1, 1, 1, 2, 5,
-        1, 1, 4, 5, 5, 1, 1, 1, 4, 2, 5, 5, 2, 2, 1, 5, 4, 1, 3, 2, 3, 2,
-         1, 4, 3, 1, 3, 4, 2, 5, 2, 2, 1, 2, 3, 1, 1, 4, 2, 5, 4, 2, 1, 1,
-          2, 3, 2, 4, 1, 1, 4, 1, 4, 2, 4, 3, 4, 4, 4, 4, 2, 5, 1, 2, 5, 2,
-           1, 4, 3, 5, 4, 4, 3, 5, 4, 1, 3, 1 ,2, 1, 5, 4, 5, 1, 3, 5, 3,
-            5, 1, 1, 3, 2, 5, 5, 2, 3, 5, 2, 1, 2, 5, 4, 1, 2, 4, 2, 2, 3,
-             5, 2, 4, 5, 1, 5, 3, 1, 4, 1, 3, 4, 5, 2, 3, 1, 3, 1, 5, 2, 2,
-              3, 3, 5, 3, 4, 5, 5, 4, 4, 4, 5, 3, 5, 1, 1, 5, 4, 3, 2, 2,
-               2, 1, 3, 1, 2, 3, 4, 2, 1, 1, 1, 3];
-keyhero.interval = [0.228, 2.407, 2.856, 3.805, 4.635, 5.161, 6.433, 6.982, 9.755, 11.118,
-    12.034, 12.548, 13.146, 13.446, 13.996, 14.361, 14.909, 15.462, 16.062, 16.631,
-     17.25, 17.753, 18.821, 19.265, 20.113, 21.21, 21.514, 21.926, 23.239, 24.287,
-      24.704, 25.616, 25.924, 28.763, 29.224, 30.553, 31.537, 31.899, 32.219, 32.815,
-       33.414, 34.096, 34.445, 34.792, 35.501, 36.163, 37.237, 37.619, 38.085, 38.55,
-        39.499, 39.781, 40.131, 40.411, 41.609, 41.989, 42.425, 43.255, 43.72, 44.268,
-         44.667, 45.019, 45.449, 45.851, 46.248, 46.598, 47.146, 47.559, 47.811, 48.141,
-          48.442, 49.008, 49.522, 49.942, 50.319, 50.571, 50.948, 51.516, 52.015, 52.448,
-           52.897, 53.246, 53.414, 54.044, 54.427, 55.84, 56.537, 56.894, 57.403, 57.669,
-            57.968, 59.468, 60.196, 60.364, 61.043, 61.391, 62.228, 62.617, 62.972, 63.372,
-             63.858, 64.623, 65.209, 65.466, 65.714, 65.997, 66.38, 66.727, 67.245, 67.543,
-              67.926, 68.341, 68.607, 68.955, 69.453, 69.771, 70.119, 70.355, 70.817, 71.002,
-               71.204, 71.739, 71.98, 72.344, 72.632, 72.834, 73.278, 73.951, 74.226, 74.709,
-                75.022, 76.155, 76.653, 77.213, 77.716, 78.384, 79.279, 80.161, 81.292, 81.824,
-                 82.151, 82.52, 82.969, 83.302, 83.685, 84.216, 85.263, 85.602, 86.178, 86.861,
-                  88.713, 89.271, 89.992, 90.4, 91.25, 92.014, 92.546, 93.351, 94.424, 94.997,
-                   95.437, 96.252, 96.634, 97.198, 98.402, 98.764, 99.214, 99.71, 100.199, 100.458,
-                    100.757, 101.112, 101.488, 101.707, 101.99, 102.173, 102.552, 103.084, 103.617,
-                     103.93, 104.465, 104.729, 105.579, 105.861, 106.063, 106.344, 106.66, 106.974,
-                      107.591, 108.17, 108.561, 108.97, 109.534, 110.035, 110.383, 110.848, 111.113,
-                       113.058, 113.574, 113.856, 114.09, 114.486, 115.017, 115.617, 116.997, 117.378,
-                        117.696, 118.162, 118.625, 118.952, 119.424, 119.691, 119.991, 120.389, 120.971,
-                         121.218, 121.818, 122.468, 122.93, 123.497, 124.329, 125.042, 125.426, 125.831,
-                          126.626, 127.138, 127.421, 127.719, 128.101, 128.402, 128.933, 129.514, 129.827,
-                           130.104, 130.496, 130.779, 131.126, 131.394, 131.843, 132.259, 132.507];
+keyhero.position;
+keyhero.interval;
 keyhero.index = 0;
 keyhero.lives = 5;
 keyhero.life = [];
@@ -51,32 +22,77 @@ var score = document.getElementById("points");
 var points = 0;
 //keyhero.runtime = Clock.getElapsedTime();
 
+
+function getMenuItemValue(item) {
+    gameName = item.innerText;
+    audioName = gameName + ".mp3";
+}
+
+
+function changeColor(bar) {
+    bar.material.opacity = 1.0;
+    keyhero.renderer.render(keyhero.scene, keyhero.camera);
+}
+
+function restoreColor(bar) {
+    bar.material.opacity = 0.5;
+    keyhero.renderer.render(keyhero.scene, keyhero.camera);
+}
+
+
+//Controller
+document.addEventListener("keyup", onDocumentKeyUp, false);
+function onDocumentKeyUp(event) {
+    var keyCode = event.which;
+    if (keyCode == 70) {        /*f*/
+        restoreColor(bar1);
+    } else if (keyCode == 71) { /*g*/
+        restoreColor(bar2);
+
+    } else if (keyCode == 72) { /*h*/
+        restoreColor(bar3);
+
+    } else if (keyCode == 74) { /*j*/
+        restoreColor(bar4);
+
+    } else if (keyCode == 75) { /*k*/
+        restoreColor(bar5);
+
+    }
+}
+
+
 //Controller
 document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
     var keyCode = event.which;
     if (keyCode == 70) {        /*f*/
         keyhero.keyPressed[0] = 1;
+        changeColor(bar1);
         setTimeout(function () {
             keyhero.keyPressed[0] = 0;
         }, 200);
     } else if (keyCode == 71) { /*g*/
         keyhero.keyPressed[1] = 1;
+        changeColor(bar2);
         setTimeout(function () {
             keyhero.keyPressed[1] = 0;
         }, 200);
     } else if (keyCode == 72) { /*h*/
         keyhero.keyPressed[2] = 1;
+        changeColor(bar3);
         setTimeout(function () {
             keyhero.keyPressed[2] = 0;
         }, 200);
     } else if (keyCode == 74) { /*j*/
         keyhero.keyPressed[3] = 1;
+        changeColor(bar4);
         setTimeout(function () {
             keyhero.keyPressed[3] = 0;
         }, 200);
     } else if (keyCode == 75) { /*k*/
         keyhero.keyPressed[4] = 1;
+        changeColor(bar5);
         setTimeout(function () {
             keyhero.keyPressed[4] = 0;
         }, 200);
@@ -93,9 +109,41 @@ keyhero.boardInit = function () {
     }
 };
 
+
+function updateComboText() {
+    const comboText = combo.toString();
+    const loader = new THREE.FontLoader();
+    loader.load('https://cdn.rawgit.com/mrdoob/three.js/r128/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+        const textGeometry = new THREE.TextGeometry(comboText, {
+            font: font,
+            size: 1,
+            height: 0.2,
+            curveSegments: 12,
+        });
+
+        const textMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+
+        // Remove the old combo text (if any)
+        const existingText = keyhero.scene.getObjectByName('comboText');
+        if (existingText) {
+            keyhero.scene.remove(existingText);
+        }
+
+        textMesh.name = 'comboText'; // Set a unique name for the textMesh
+        keyhero.scene.add(textMesh);
+    });
+}
+
+
 keyhero.addScore = function (pts) {
     points += pts;
     keyhero.updateScore();
+
+    combo++;
+    updateComboText();
+
+    keyhero.renderer.render(keyhero.scene, keyhero.camera);
 };
 
 keyhero.updateScore = function () {
@@ -106,7 +154,10 @@ keyhero.updateScore = function () {
 keyhero.init = function () {
     $("#play").hide();
     $("#text").hide();
+    $(".vertical-menu").hide();
     $("#score").show();
+    //$("#combo").show();
+
 
     //Scene
     keyhero.scene = new THREE.Scene();
@@ -119,23 +170,23 @@ keyhero.init = function () {
     keyhero.camera.rotation.x = -89;
     keyhero.scene.add(keyhero.camera);
 
-    var bar1 = new THREE.Mesh(new THREE.BoxGeometry(1.3, 0.5, .6), new THREE.MeshBasicMaterial({ color: keyhero.colors[0], transparent: true, opacity: 0.5 }));
+    bar1 = new THREE.Mesh(new THREE.BoxGeometry(1.3, 0.5, .6), new THREE.MeshBasicMaterial({ color: keyhero.colors[0], transparent: true, opacity: 0.5 }));
     bar1.position.x = -3;
     bar1.position.y = 0;
     bar1.position.z = 7.2;
-    var bar2 = new THREE.Mesh(new THREE.BoxGeometry(1.3, 1.2, .5), new THREE.MeshBasicMaterial({ color: keyhero.colors[1], transparent: true, opacity: 0.5 }));
+    bar2 = new THREE.Mesh(new THREE.BoxGeometry(1.3, 1.2, .5), new THREE.MeshBasicMaterial({ color: keyhero.colors[1], transparent: true, opacity: 0.5 }));
     bar2.position.x = -1.4;
     bar2.position.y = 0;
     bar2.position.z = 7.2;
-    var bar3 = new THREE.Mesh(new THREE.BoxGeometry(1.3, 1.4, .5), new THREE.MeshBasicMaterial({ color: keyhero.colors[2], transparent: true, opacity: 0.5 }));
+    bar3 = new THREE.Mesh(new THREE.BoxGeometry(1.3, 1.4, .5), new THREE.MeshBasicMaterial({ color: keyhero.colors[2], transparent: true, opacity: 0.5 }));
     bar3.position.x = 0;
     bar3.position.y = 0;
     bar3.position.z = 7.2;
-    var bar4 = new THREE.Mesh(new THREE.BoxGeometry(1.3, 1.2, .5), new THREE.MeshBasicMaterial({ color: keyhero.colors[3], transparent: true, opacity: 0.5 }));
+    bar4 = new THREE.Mesh(new THREE.BoxGeometry(1.3, 1.2, .5), new THREE.MeshBasicMaterial({ color: keyhero.colors[3], transparent: true, opacity: 0.5 }));
     bar4.position.x = 1.4;
     bar4.position.y = 0;
     bar4.position.z = 7.2;
-    var bar5 = new THREE.Mesh(new THREE.BoxGeometry(1.3, 0.5, .6), new THREE.MeshBasicMaterial({ color: keyhero.colors[4], transparent: true, opacity: 0.5 }));
+    bar5 = new THREE.Mesh(new THREE.BoxGeometry(1.3, 0.5, .6), new THREE.MeshBasicMaterial({ color: keyhero.colors[4], transparent: true, opacity: 0.5 }));
     bar5.position.x = 3;
     bar5.position.y = 0;
     bar5.position.z = 7.2;
@@ -200,17 +251,41 @@ keyhero.init = function () {
 
     keyhero.boardInit();
 
-    keyhero.game();
+    fetch("./build/" + gameName + ".json")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+
+            keyhero.position = data.position;
+            keyhero.interval = data.interval;
+            songFlag = 0;
+            interval = keyhero.interval[0] * 1000;
+            console.log(keyhero.position.length);
+            keyhero.game();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
 };
-var songFlag = 0;
-var interval = keyhero.interval[0] * 1000;
-console.log(keyhero.position.length);
+var songFlag;
+var interval;
+
 keyhero.game = function () {
-    if(songFlag === 0){
-        document.getElementById("gachon").play();
+
+
+    if (songFlag === 0) {
+        document.getElementById("audio").setAttribute('src', '/build/' + audioName);
+        document.getElementById("audio").load();
+        document.getElementById("audio").play();
         songFlag = 1;
     }
-        
+
+
     if (!keyhero.gameover) {
 
         setTimeout(function () {
@@ -236,6 +311,7 @@ keyhero.game = function () {
                     if (keyhero.keyPressed[note.x] == 1 && note.position.z >= 7.1) {
 
                         keyhero.addScore(20);
+                        
 
                         keyhero.scene.remove(note);
                         cancelAnimationFrame(move);
@@ -250,6 +326,10 @@ keyhero.game = function () {
                     keyhero.lives -= 1;
                     keyhero.scene.remove(keyhero.life[keyhero.lives]);
 
+
+                    combo =0;
+                    updateComboText();
+
                     keyhero.scene.remove(note);
                     keyhero.renderer.render(keyhero.scene, keyhero.camera);
                     cancelAnimationFrame(move);
@@ -257,22 +337,26 @@ keyhero.game = function () {
                     note = undefined;
                     move = undefined;
 
-                    // if (keyhero.lives == 0) {
-                    //     window.location.replace("gameOver.html");
-                    // }
+                     if (keyhero.lives == 0) {
+                         window.location.replace("gameOver.html");
+                    }
                 }
             };
 
             move();
-            if(keyhero.index === keyhero.interval.length){
-                setTimeout(function() {window.location.replace("gameOver.html")}, 7000);
-                
+
+        
+
+
+            if (keyhero.index === keyhero.interval.length) {
+                setTimeout(function () { window.location.replace("gameOver.html") }, 7000);
+
                 return;
-            } else{
+            } else {
                 interval = (keyhero.interval[keyhero.index + 1] - keyhero.interval[keyhero.index]) * 1000;
                 keyhero.index++;
             }
-        
+
             //recursive call
             keyhero.game();
 
@@ -285,3 +369,4 @@ keyhero.game = function () {
     }
 
 };
+
