c0b4ac3bef9ac7fcf7ea28c6add7630693fef367
youhyunwoo99 <casshyunwoo@naver.com>
2023-11-10 22:01:56 +0900

    마우스게임 상자색깔 변경

--- MouseTouchGame/MousetouchGame.js
+++ MouseTouchGame/MousetouchGame.js
@@ -1,6 +1,6 @@
 let count = 0;
 let topScore = localStorage.getItem("topScore") || 0;
-document.getElementById('topScore').textContent = topScore;  // Update top score at the beginning
+document.getElementById('topScore').textContent = topScore;
 
 const startScreen = document.getElementById('startScreen');
 const startButton = document.getElementById('startButton');
@@ -8,59 +8,59 @@ const gameDiv = document.getElementById('game');
 const timerDisplay = document.getElementById('timer');
 const counterDisplay = document.getElementById('counter');
 const topScoreDisplay = document.getElementById('topScore'); 
-const restartButton=document.getElementById('restartButton');
-const homeButton=document.getElementById('homeButton');
-const gameOverDiv=document.getElementById('gameOver');
+const restartButton = document.getElementById('restartButton');
+const homeButton = document.getElementById('homeButton');
+const gameOverDiv = document.getElementById('gameOver');
 
 startButton.addEventListener('click', () => {
-    startScreen.style.display='none';
-    gameDiv.style.display='block';
-    gameOverDiv.style.display='none'; // Hide Game Over Screen at the start of Game
+    startScreen.style.display = 'none';
+    gameDiv.style.display = 'block';
+    gameOverDiv.style.display = 'none';
 
-    let timeLeft=50;
-    const timerId=setInterval(() => {
-        timeLeft--;
-        timerDisplay.textContent=timeLeft;
+    let timeLeft = 10000; // Change the initial time to 10 seconds (in milliseconds)
+    const timerId = setInterval(() => {
+        timeLeft -= 100; // Adjust the interval to 100ms (0.1 seconds)
+        timerDisplay.textContent = (timeLeft / 1000).toFixed(1); // Display time in seconds with one decimal place
 
-        if(timeLeft <= 0) {
+        if (timeLeft <= 0) {
             clearInterval(timerId);
-            window.removeEventListener('click', onMouseClick); // Remove the click event listener when the game ends.
+            window.removeEventListener('click', onMouseClick);
             alert(`Time's up! You clicked ${count} times.`);
-	        
-            if(count > topScore) { 
+            
+            if (count > topScore) { 
                 topScore = count;
                 localStorage.setItem("topScore", topScore);
                 document.getElementById('topScore').textContent = topScore;
             }
-	        count=0;
-	        counterDisplay.textContent=count;
+            count = 0;
+            counterDisplay.textContent = count;
 
-	        gameOverDiv.style.display='block'; // Showing the game Over screen
+            gameOverDiv.style.display = 'block';
         }
-    }, 100);
+    }, 100); // Adjust the interval to 100ms (0.1 seconds)
 
-window.addEventListener('click', onMouseClick); // Add a click event listener when the game starts.
+    window.addEventListener('click', onMouseClick);
 });
 
 function incrementCounter() {
-	count++;
-	counterDisplay.textContent=count;
+    count++;
+    counterDisplay.textContent = count;
 }
 
 function onMouseClick(event) {
-	raycaster.setFromCamera(mouse,camera);
+    raycaster.setFromCamera(mouse, camera);
 
-	var intersects=raycaster.intersectObjects(scene.children,true);
+    var intersects = raycaster.intersectObjects(scene.children, true);
 
-	if(intersects.length >0){
-	    incrementCounter(); 
-	    console.log("You clicked on the box!");
-	}
+    if (intersects.length > 0) {
+        incrementCounter(); 
+        console.log("You clicked on the box!");
+    }
 }
 
 // Added: Restart button click event handler
 restartButton.addEventListener("click", () => {
-	startScreen.style.display="none";
-	gameOverDiv.style.display="none";
-	startButton.click(); // When you click the Restart button, the Start button click event occurs
+    startScreen.style.display = "none";
+    gameOverDiv.style.display = "none";
+    startButton.click();
 });
--- MouseTouchGame/background.js
+++ MouseTouchGame/background.js
@@ -1,5 +1,5 @@
 let scene, camera, renderer;
-let geometry, material, mesh;
+let geometry, materials, mesh;
 let raycaster = new THREE.Raycaster();
 let mouse = new THREE.Vector2();
 
@@ -13,44 +13,57 @@ function init() {
     camera.position.z = 5;
 
     geometry = new THREE.BoxGeometry(2.5, 2.5, 2.5);
-	material = new THREE.MeshBasicMaterial({ color: "#0000CD" });
 
-    mesh = new THREE.Mesh(geometry, material);
+    // Define an array of materials with different colors
+    materials = [
+        new THREE.MeshBasicMaterial({ color: 0x8a2be2 }),  // Violet
+        new THREE.MeshBasicMaterial({ color: 0xffff00 }),  // Yellow
+        new THREE.MeshBasicMaterial({ color: 0xff0000 }),  // Red
+        new THREE.MeshBasicMaterial({ color: 0x0000ff }),  // Blue
+        new THREE.MeshBasicMaterial({ color: 0xffa500 }),  // Orange
+        new THREE.MeshBasicMaterial({ color: 0x008000 }),  // Green
+        new THREE.MeshBasicMaterial({ color: 0xffc0cb }),  // Pink
+        new THREE.MeshBasicMaterial({ color: 0xffffff }),  // White
+        new THREE.MeshBasicMaterial({ color: 0x000080 }),  // Navy
+        new THREE.MeshBasicMaterial({ color: 0xa52a2a })   // Brown
+    ];
+
+    mesh = new THREE.Mesh(geometry, materials);
     scene.add(mesh);
 
-	renderer = new THREE.WebGLRenderer({ antialias:true });
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     
-	renderer.setSize(window.innerWidth , window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-	animate();
+    animate();
 
-	// Add event listener for mouse move
-	window.addEventListener('mousemove', onMouseMove);
+    // Add event listener for mouse move
+    window.addEventListener('mousemove', onMouseMove);
 
-	// Add event listener for mouse click
-	window.addEventListener('click', onMouseClick);
+    // Add event listener for mouse click
+    window.addEventListener('click', onMouseClick);
 }
 
 function animate() {
-	requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.01;
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
 
-	renderer.render(scene,camera);
+    renderer.render(scene, camera);
 }
 
 function onMouseMove(event) {
-	mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-	mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
 function onMouseClick() {
-	raycaster.setFromCamera(mouse,camera);
+    raycaster.setFromCamera(mouse, camera);
 
-	var intersects=raycaster.intersectObjects(scene.children,true);
+    var intersects = raycaster.intersectObject(mesh);
 
-	if(intersects.length >0){
-		console.log("You clicked on the box!");
-	}
+    if (intersects.length > 0) {
+        console.log("You clicked on the box!");
+    }
 }
